49e21b84e6679890e97e819d7e36e8f8b7a70c71
navnit56 <navnit.kaklotar@magecomp.com>
2026-05-06 12:57:13 +0530

    Create connection1.php for database connection

---
+++ connection1.php
@@ -0,0 +1,16 @@
+<!-- connection.php -->
+
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "student_db";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection Failed : " . $conn->connect_error);
+}
+
+?>
